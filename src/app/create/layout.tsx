03fca179ff39955578e485d1e11a820cf0465509
bbarkthong <bbarkthong@gmail.com>
2023-08-13 16:43:24 +0900

    생활코딩 완료

--- src/app/create/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function RootLayout(props) {
-    return (
-        <form>
-            <h2>Create</h2>
-            {props.children}
-        </form>
-    )
-}
--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-
-export default function Read(props) {
+export default  async function Read(props) {
+    const resp = await fetch(process.env.NEXT_PUBLIC_API_URL+ 'topics/${props.params.id}', {cache: 'no-store'});
+    const topic = await resp.json();
     return (
         <>
-            <h2>Read</h2>
-            parameters : {props.params.id}
+            <h2>{topic.title}</h2>
+            {topic.body}
         </>
     )
 }
